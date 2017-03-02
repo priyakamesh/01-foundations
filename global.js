6ec4deb5ee3e2f1efb18ed1d909fcdacb68c8a39
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-02 15:48:20 -0600

    bonus steps are done in global.js

--- global.js
+++ global.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-
-console.log("Node.js version",process.version)
-console.log("V8 version", process.versions.v8)
-
-
-const {versionDisplay}= require('process')
-// console.log(global)
+// console.log(process)
+console.log("Node.js version",`${process.version}\n`,"V8 version", process.versions.v8)
+const {version}= process
+const {versions} = process
+console.log('Node.js version ',`${version}\n`,'v8 version', `${versions.v8}`)
