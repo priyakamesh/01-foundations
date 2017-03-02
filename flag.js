a2708ebc7cd4e3bc2de1daf1cf0cb513982b3583
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-02 15:12:54 -0600

    in flag.js added code in for loop

--- flag.js
+++ flag.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 const chalk = require("chalk")
+for (let i=0; i < 3; i++){
+  console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
+  console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgWhite('                                                      ')}`)
+}
 console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgWhite('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgWhite('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgWhite('                                                      ')}`)
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
-console.log(`${chalk.bgWhite('                                                                       ')}`)
-console.log(`${chalk.bgRed('                                                                       ')}`)
-console.log(`${chalk.bgWhite('                                                                       ')}`)
-console.log(`${chalk.bgRed('                                                                       ')}`)
-console.log(`${chalk.bgWhite('                                                                       ')}`)
-console.log(`${chalk.bgRed('                                                                       ')}`)
+for (let i =0; i< 3; i++){
+  console.log(`${chalk.bgWhite('                                                                       ')}`)
+  console.log(`${chalk.bgRed('                                                                       ')}`)
+
+}
