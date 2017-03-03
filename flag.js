036bb409063910229a08cbf003d5523d144e1368
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-03 09:28:56 -0600

    changed the spacing to array

--- flag.js
+++ flag.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
+const space = " ".repeat(47)
+const space1 = " ".repeat(65)
 const chalk = require("chalk")
 for (let i=0; i < 3; i++){
-  console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
-  console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgWhite('                                                      ')}`)
+  console.log(`${chalk.bgBlue.bold(' *  *  *  *  *  * ')}${chalk.bgRed(`${space}`)}`)
+  console.log(`${chalk.bgBlue.bold('   *  *  *  *  *  ')}${chalk.bgWhite(`${space}`)}`)
 }
-console.log(`${chalk.bgBlue.bold(' * * * * * * * * ')}${chalk.bgRed('                                                      ')}`)
+console.log(`${chalk.bgBlue.bold(' *  *  *  *  *  * ')}${chalk.bgRed(`${space}`)}`)
 for (let i =0; i< 3; i++){
-  console.log(`${chalk.bgWhite('                                                                       ')}`)
-  console.log(`${chalk.bgRed('                                                                       ')}`)
+  console.log(`${chalk.bgWhite(`${space1}`)}`)
+  console.log(`${chalk.bgRed(`${space1}`)}`)
 
 }
